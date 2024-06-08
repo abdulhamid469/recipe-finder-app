5bb29e333714bd73aafae6fca1d90e5fbeeeaf68
Abdul Hamid <hamidkhattak469@gmail.com>
2024-06-08 15:04:19 +0500

    All basic setup is completed like install taiwindcss, daisyUI etc

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn btn-active">Default</button>
+<button className="btn btn-active btn-neutral">Neutral</button>
+<button className="btn btn-active btn-primary">Primary</button>
+<button className="btn btn-active btn-secondary">Secondary</button>
+<button className="btn btn-active btn-accent">Accent</button>
+<button className="btn btn-active btn-ghost">Ghost</button>
+<button className="btn btn-active btn-link">Link</button>
+    </div>
+  )
+}
+
+export default App;
